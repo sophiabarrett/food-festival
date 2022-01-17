225c7951e16cbb6439ebd0cae6fabfb946fb3a01
Sophia Barrett <sophia@yoursummit.media>
2022-01-17 16:32:57 -0700

    initial webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,16 @@ const path = require("path");
 module.exports = {
   // add basic configuration (entry, output, mode)
   // entry point is the root of the bundle and the beginning of the dependency graph
+  // entry is where webpack looks to start building the module
   entry: "./assets/js/script.js",
   // webpack will take the provided entry point, bundle the code, and output the bundled code to this folder
+  // output is used to tell webpack where the files are going to go and what are the names of those files
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
   },
   // setup the mode in which we want webpack to run
-  // webpack runs in 'production mode' automatically, which will automatically minify our code
+  // webpack runs in 'production mode' automatically, which will automatically minify our code and run uglify(?)
+  // development mode will offer hot reloading of webpack and debugging features
   mode: "development",
 };
