6f1b360a43e80bec83f0167d83a09126d95285e1
Sophia Barrett <sophia@yoursummit.media>
2022-01-17 17:41:30 -0700

    bundle js and add multiple entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-// add Node.js path module
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
 
@@ -7,18 +8,26 @@ module.exports = {
   // add basic configuration (entry, output, mode)
   // entry point is the root of the bundle and the beginning of the dependency graph
   // entry is where webpack looks to start building the module
-  entry: "./assets/js/script.js",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
+  },
   // webpack will take the provided entry point, bundle the code, and output the bundled code to this folder
   // output is used to tell webpack where the files are going to go and what are the names of those files
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+    filename: "[name].bundle.js",
   },
-  // add plugin to make exception for jQuery's $
+  // add plugins
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
     }),
   ],
   // setup the mode in which we want webpack to run
