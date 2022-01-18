275c80eb364573c662ed84cb2d870ad7d15c4354
Sophia Barrett <sophia@yoursummit.media>
2022-01-18 11:36:40 -0700

    register and install service worker

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ const webpack = require("webpack");
 
 // create main configuration object
 module.exports = {
+  // setup dev server
+  devServer: {
+    static: {
+      directory: __dirname,
+    },
+    compress: true,
+    port: 8080,
+  },
   // add basic configuration (entry, output, mode)
   // entry point is the root of the bundle and the beginning of the dependency graph
   // entry is where webpack looks to start building the module
